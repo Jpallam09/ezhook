54358d89845e460dc99c2206daced36bc18fcee1
jpvrcm <allamjohnpaul0901@gmail.com>
2026-05-25 21:40:30 +0800

    refactor: extract HeroSection component

---
+++ src/app/pages/components/HeroSection/index.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+export function HeroSection() {
+  return (
+    <div className="mt-60 w-full">
+      <h2 className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold md:text-4xl lg:text-7xl">
+        Interactive Background Boxes Ripple Effect
+      </h2>
+
+      <p className="relative z-10 mx-auto mt-4 max-w-xl text-center text-muted-foreground">
+        Hover over the boxes above and click. To be used on backgrounds of
+        hero sections OR Call to Action sections.
+      </p>
+    </div>
+  );
+}
